2d13c6572ad2a0f6773c6bd7f031c1b4497903d5
stefan <stefan@stefan-isele@prefabware.de>
2017-11-12 14:26:35 +0100

    folders and poms

---
+++ commons-spring/src/main/java/com/prefabware/tec/commons/spring/App.java
@@ -0,0 +1,13 @@
+package com.prefabware.tec.commons.spring;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
